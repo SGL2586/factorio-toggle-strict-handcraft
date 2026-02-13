44c108faeb462b4cc115141e8c5c505dde9efdf6
SGL2586 <49787600+SGL2586@users.noreply.github.com>
2026-02-12 21:50:11 -0600

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,19 @@
+data:extend({
+  {
+    type = "shortcut",
+    name = "strict-handcraft-toggle",
+    action = "lua",
+    toggleable = true,
+    associated_control_input = nil,
+    icon = {
+      filename = "__base__/graphics/icons/assembling-machine-1.png",
+      size = 64,
+      scale = 1
+    },
+    small_icon = {
+      filename = "__base__/graphics/icons/assembling-machine-1.png",
+      size = 64,
+      scale = 0.5
+    }
+  }
+})
