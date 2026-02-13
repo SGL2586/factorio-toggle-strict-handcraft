7ecf1a171668c2accaf87e234e0ca349e6068c88
SGL2586 <49787600+SGL2586@users.noreply.github.com>
2026-02-12 21:49:20 -0600

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+  {
+    type = "string-setting",
+    name = "strict-handcraft-items",
+    setting_type = "runtime-per-user",
+    default_value = "copper-cable,electronic-circuit,iron-gear-wheel,iron-stick",
+    allow_blank = true
+  }
+})
