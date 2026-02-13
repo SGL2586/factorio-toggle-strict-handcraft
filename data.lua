4c0fac1e42565b351503253115d74b4a79b65231
SGL2586 <49787600+SGL2586@users.noreply.github.com>
2026-02-13 11:39:37 -0600

    Update data.lua

--- data.lua
+++ data.lua
@@ -4,16 +4,7 @@ data:extend({
     name = "strict-handcraft-toggle",
     action = "lua",
     toggleable = true,
-    associated_control_input = nil,
-    icon = {
-      filename = "__base__/graphics/icons/assembling-machine-1.png",
-      size = 64,
-      scale = 1
-    },
-    small_icon = {
-      filename = "__base__/graphics/icons/assembling-machine-1.png",
-      size = 64,
-      scale = 0.5
-    }
+    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    small_icon = "__base__/graphics/icons/assembling-machine-1.png"
   }
 })
